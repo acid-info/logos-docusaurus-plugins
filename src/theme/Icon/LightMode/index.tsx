8f0869d83e4999683467846f60eeaebbd10a75ee
Hossein Mehrabi <ohyeaross@gmail.com>
2022-10-07 19:50:24 +0330

    update light mode icon.

---
+++ src/theme/Icon/LightMode/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import type { Props } from '@theme/Icon/LightMode'
+import Icon from '@site/static/icons/day.svg'
+
+export default function IconLightMode(props: Props): JSX.Element {
+  return <Icon {...props} />
+}
