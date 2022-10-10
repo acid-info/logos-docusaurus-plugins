b64a5f1d30e29bd6add9e6fbdb70ff9e67ec7036
Hossein Mehrabi <ohyeaross@gmail.com>
2022-10-10 19:31:27 +0330

    keep sidebar closed by default on landing pages.

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -6,7 +6,7 @@ import './styles.scss'
 
 export default function LayoutWrapper(props) {
   const isIndexPage = useIsVersionIndexPage()
-  const store = globalStore.useCreateStore({ hiddenSidebar: false })
+  const store = globalStore.useCreateStore({ hiddenSidebar: isIndexPage })
 
   return (
     <globalStore.Provider store={store}>
