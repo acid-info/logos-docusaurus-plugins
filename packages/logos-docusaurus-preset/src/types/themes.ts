e0bb0044732a0fb865ff99bcce828e68629cac8b
Hossein Mehrabi <ohyeaross@gmail.com>
2022-10-28 11:54:40 +0330

    export theme types.

--- packages/logos-docusaurus-preset/src/types/themes.ts
+++ packages/logos-docusaurus-preset/src/types/themes.ts
@@ -6,4 +6,4 @@ export type ThemeConfig = {
   }
 }
 
-export type { PluginOptions as DefaultThemeOptions } from '@acid-info/logos-docusaurus-theme/src'
+export type { PluginOptions as DefaultThemeOptions } from '@acid-info/logos-docusaurus-theme'
