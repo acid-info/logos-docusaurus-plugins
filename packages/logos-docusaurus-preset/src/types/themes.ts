e0f47d8ccd5938cb35ed28ae63603b770c867fc7
Hossein Mehrabi <ohyeaross@gmail.com>
2023-03-17 14:30:53 +0330

    feat: make theme configuration more customizable

--- packages/logos-docusaurus-preset/src/types/themes.ts
+++ packages/logos-docusaurus-preset/src/types/themes.ts
@@ -1,12 +1,8 @@
-import { NavbarItem, Footer } from '@docusaurus/theme-common'
+import { Footer, Navbar } from '@docusaurus/theme-common'
 
 export type ThemeConfig = {
-  navbar?: {
-    items?: NavbarItem[]
-  }
-  footer?: {
-    links?: Footer['links']
-  }
+  navbar?: Navbar
+  footer?: Footer
 }
 
 export type { PluginOptions as DefaultThemeOptions } from '@acid-info/logos-docusaurus-theme'
