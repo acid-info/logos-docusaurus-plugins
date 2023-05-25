d565c17243cf4d2856b6491f2bf7ff4275029231
jinhojang6 <jinho5051@gmail.com>
2023-05-26 03:42:19 +0900

    update footer css

--- packages/logos-docusaurus-theme/src/client/theme/Footer/LinkItem/index.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/Footer/LinkItem/index.tsx
@@ -3,13 +3,17 @@ import Link from '@docusaurus/Link'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import isInternalUrl from '@docusaurus/isInternalUrl'
 import IconExternalLink from '@theme/Icon/ExternalLink'
+import styles from './style.module.scss'
+import clsx from 'clsx'
+
 export default function FooterLinkItem({ item }) {
   const { to, href, label, prependBaseUrlToHref, ...props } = item
   const toUrl = useBaseUrl(to)
   const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true })
+
   return (
     <Link
-      className="footer__link-item"
+      className={clsx('footer__link-item', styles.linkItem)}
       {...(href
         ? {
             href: prependBaseUrlToHref ? normalizedHref : href,
