6250b7ce61fe1ec4a9dd16d3f7ecdeec78022362
jinhojang6 <jinho5051@gmail.com>
2023-06-01 05:12:36 +0900

    add back to top button

--- packages/logos-docusaurus-theme/src/client/theme/Footer/Layout/index.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/Footer/Layout/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import clsx from 'clsx'
 import styles from './styles.module.scss'
+import { Button } from '@acid-info/lsd-react'
 
 export default function FooterLayout({ style, links, logo, copyright }) {
   const firstRow = [...links.props.links.slice(0, 2)]
   const secondRow = [...links.props.links.slice(2)]
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
 
   return (
     <footer
@@ -27,6 +35,13 @@ export default function FooterLayout({ style, links, logo, copyright }) {
         <div className="footer__bottom text--center">Logos Collective</div>
         {React.cloneElement(links, { links: secondRow })}
       </div>
+      <Button
+        className={styles.backToTop}
+        size="small"
+        onClick={handleScrollToTop}
+      >
+        Back to top ↑
+      </Button>
     </footer>
   )
 }
