b6a1a19780b9a80ff3dc30dc442d36b76ac021a7
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-01 15:04:40 +0330

    fix: render hero model only in browser

--- packages/logos-docusaurus-theme/src/client/components/mdx/HeroModel/index.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/HeroModel/index.tsx
@@ -1,1 +1,17 @@
-export * from './HeroModel'
+import BrowserOnly from '@docusaurus/BrowserOnly'
+import React from 'react'
+import { HeroModelProps } from './HeroModel'
+
+export const HeroModel: React.FC<HeroModelProps> = (props) => {
+  return (
+    <BrowserOnly>
+      {() => {
+        const { HeroModel: Model } = require('./HeroModel')
+
+        return <Model {...props}></Model>
+      }}
+    </BrowserOnly>
+  )
+}
+
+export type { HeroModelProps } from './HeroModel'
