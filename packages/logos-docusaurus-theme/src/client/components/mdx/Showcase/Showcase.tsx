a00195fbbc7e8c4fd6befd0231e90d9f179380f4
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-02 14:59:40 +0330

    feat: support themed images for showcase mdx component

--- packages/logos-docusaurus-theme/src/client/components/mdx/Showcase/Showcase.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/Showcase/Showcase.tsx
@@ -1,11 +1,13 @@
 import { Typography } from '@acid-info/lsd-react'
+import { useColorMode } from '@docusaurus/theme-common'
 import clsx from 'clsx'
 import React from 'react'
 import './Showcase.scss'
 
 export type ShowcaseItem = {
   name: React.ReactNode
-  logo: any
+  logo: string
+  logoDark: string
   description: React.ReactNode
 }
 
@@ -22,33 +24,39 @@ export const Showcase: React.FC<ShowcaseProps> = ({
   children,
   ...props
 }) => {
+  const { isDarkTheme } = useColorMode()
+
   return (
     <div className={clsx(className, 'mdx-showcase')} {...props}>
-      {items.map((item, index) => (
-        <div key={index} className="mdx-showcase__item">
-          <div key={index} className="mdx-showcase__item-inner">
-            <img
-              src={item.logo}
-              alt={typeof item.name === 'string' ? item.name : 'image'}
-              className="mdx-showcase__item-logo"
-            />
-            <Typography
-              variant="h3"
-              component="h2"
-              className="mdx-showcase__item-name"
-            >
-              {item.name}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="p"
-              className="mdx-showcase__item-description"
-            >
-              {item.description}
-            </Typography>
+      {items.map((item, index) => {
+        const logoSrc = item.logo ?? item.logoDark
+
+        return (
+          <div key={index} className="mdx-showcase__item">
+            <div key={index} className="mdx-showcase__item-inner">
+              <img
+                src={(isDarkTheme ? item.logoDark : item.logo) ?? logoSrc}
+                alt={typeof item.name === 'string' ? item.name : 'image'}
+                className="mdx-showcase__item-logo"
+              />
+              <Typography
+                variant="h3"
+                component="h2"
+                className="mdx-showcase__item-name"
+              >
+                {item.name}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                component="p"
+                className="mdx-showcase__item-description"
+              >
+                {item.description}
+              </Typography>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
