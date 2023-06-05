4859e36dab941cc705d140c128728b7886a0309d
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-05 22:51:19 +0330

    fix: use lsd's default theme in landing page

--- packages/logos-docusaurus-theme/src/client/theme/Root.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/Root.tsx
@@ -1,23 +1,33 @@
 import { defaultThemes, ThemeProvider } from '@acid-info/lsd-react'
+import { useActivePlugin } from '@docusaurus/plugin-content-docs/lib/client/index.js'
 import {
   ColorModeProvider,
   useColorMode,
 } from '@docusaurus/theme-common/internal'
 import React from 'react'
+import { darkTheme, lightTheme } from '../lib/themes'
 import { useDocThemeOptions } from '../lib/useThemeOptions'
 import styles from './style.module.css'
-import { lightTheme, darkTheme } from '../lib/themes'
+
+const docThemes = {
+  light: lightTheme,
+  dark: darkTheme,
+}
+
+const useTheme = () => {
+  const colorMode = useColorMode()
+  const activePlugin = useActivePlugin()
+
+  return (activePlugin ? docThemes : defaultThemes)[colorMode.colorMode]
+}
 
 const Content = ({ children }) => {
-  const colorMode = useColorMode()
-
+  const theme = useTheme()
   const options = useDocThemeOptions()
   const hideDocSidebar = options?.sidebar?.hide
 
   return (
-    <ThemeProvider
-      theme={colorMode.colorMode === 'dark' ? darkTheme : lightTheme}
-    >
+    <ThemeProvider theme={theme}>
       <div className={styles.root} data-hidden-doc-sidebar={hideDocSidebar}>
         {children}
       </div>
