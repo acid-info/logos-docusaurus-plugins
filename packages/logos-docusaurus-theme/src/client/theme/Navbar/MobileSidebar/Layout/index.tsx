f8d720a6f4bb339096d5144002f9ce45690ced91
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-07 18:46:20 +0330

    fix: mobile sidebar layout and spacing

--- packages/logos-docusaurus-theme/src/client/theme/Navbar/MobileSidebar/Layout/index.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/Navbar/MobileSidebar/Layout/index.tsx
@@ -18,10 +18,12 @@ export default function NavbarMobileSidebarLayout({
           'navbar-sidebar__items--show-secondary': secondaryMenuShown,
         })}
       >
-        <div className="navbar-sidebar__item menu">{primaryMenu}</div>
+        <div className="navbar-sidebar__item menu">
+          {primaryMenu}
+          {!secondaryMenuShown && <Footer />}
+        </div>
         <div className="navbar-sidebar__item menu">{secondaryMenu}</div>
       </div>
-      {!secondaryMenuShown && <Footer />}
     </div>
   )
 }
