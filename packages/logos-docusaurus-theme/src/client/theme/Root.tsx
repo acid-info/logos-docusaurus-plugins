e9308bd9f6e58a245523f451b2e150a3eaba8528
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-12 03:33:29 +0330

    fix: prevent screen flash by loading css vars for both dark and light theme in initial load

--- packages/logos-docusaurus-theme/src/client/theme/Root.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/Root.tsx
@@ -1,10 +1,15 @@
-import { defaultThemes, ThemeProvider } from '@acid-info/lsd-react'
+import {
+  defaultThemes,
+  Theme,
+  ThemeProvider as LSDThemeProvider,
+} from '@acid-info/lsd-react'
 import { useActivePlugin } from '@docusaurus/plugin-content-docs/lib/client/index.js'
 import {
   ColorModeProvider,
   useColorMode,
 } from '@docusaurus/theme-common/internal'
-import React from 'react'
+import { css, Global } from '@emotion/react'
+import React, { useMemo } from 'react'
 import { darkTheme, lightTheme } from '../lib/themes'
 import { useDocThemeOptions } from '../lib/useThemeOptions'
 import styles from './style.module.css'
@@ -18,16 +23,46 @@ const useTheme = () => {
   const colorMode = useColorMode()
   const activePlugin = useActivePlugin()
 
-  return (activePlugin ? docThemes : defaultThemes)[colorMode.colorMode]
+  const themes = activePlugin ? docThemes : defaultThemes
+
+  return {
+    dark: themes.dark,
+    light: themes.light,
+    current: themes[colorMode.colorMode],
+    colorMode: colorMode.colorMode,
+  }
+}
+
+const useThemeCssVars = (theme: Theme, light: boolean = false) =>
+  useMemo(
+    () => css`
+      [data-theme='${light ? 'light' : 'dark'}'] {
+        ${theme.cssVars}
+      }
+    `,
+    [theme],
+  )
+
+const ThemeProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const theme = useTheme()
+  const darkCssVars = useThemeCssVars(theme.dark, false)
+  const lightCssVars = useThemeCssVars(theme.light, true)
+
+  return (
+    <LSDThemeProvider theme={theme.current} injectCssVars={false}>
+      {children}
+      <Global styles={darkCssVars} />
+      <Global styles={lightCssVars} />
+    </LSDThemeProvider>
+  )
 }
 
 const Content = ({ children }) => {
-  const theme = useTheme()
   const options = useDocThemeOptions()
   const hideDocSidebar = options?.sidebar?.hide
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <div className={styles.root} data-hidden-doc-sidebar={hideDocSidebar}>
         {children}
       </div>
