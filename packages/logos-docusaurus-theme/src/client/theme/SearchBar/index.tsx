339d5bcd912d3a4afcead8aaeb3390417c75e7b4
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-13 17:54:30 +0330

    fix: render search bar component regardless of whether any active plugins are found

--- packages/logos-docusaurus-theme/src/client/theme/SearchBar/index.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/SearchBar/index.tsx
@@ -1,12 +1,8 @@
 import ErrorBoundary from '@docusaurus/ErrorBoundary'
-import { useActivePlugin } from '@docusaurus/plugin-content-docs/lib/client/index.js'
 import React from 'react'
 import { SearchBar } from './SearchBar'
 
 const SearchBarWrapper: React.FC = () => {
-  const activePlugin = useActivePlugin()
-  if (!activePlugin) return <></>
-
   return typeof window === 'undefined' ? (
     <></>
   ) : (
