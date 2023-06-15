cb4452be4e697788522ad498d06fd6d68b5289fe
jinhojang6 <jinho5051@gmail.com>
2023-06-15 23:36:44 +0900

    fix a bug with collapsible sidebar

--- packages/logos-docusaurus-theme/src/client/theme/DocSidebarItem/Category/index.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/DocSidebarItem/Category/index.tsx
@@ -150,7 +150,7 @@ export default function DocSidebarItemCategory({
               ? (e) => {
                   onItemClick?.(item)
                   if (href) {
-                    updateCollapsed(false)
+                    updateCollapsed()
                   } else {
                     e.preventDefault()
                     updateCollapsed()
