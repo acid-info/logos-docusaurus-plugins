92d9824ad9b0bf04acb079db43db208870936707
Hossein Mehrabi <ohyeaross@gmail.com>
2023-07-07 17:23:43 +0330

    feat: refactor CTA component and implement list variant; fixes #93

--- packages/logos-docusaurus-theme/src/client/components/mdx/CallToActionSection/CallToActionSection.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/CallToActionSection/CallToActionSection.tsx
@@ -15,57 +15,77 @@ export type CallToActionSectionProps = Omit<
   label?: string
   target?: React.AnchorHTMLAttributes<HTMLAnchorElement>['target']
   variant?: ButtonProps['variant']
+  list?: {
+    title: React.ReactNode
+    description: React.ReactNode
+  }[]
 }
 
 export const CallToActionSection: React.FC<CallToActionSectionProps> = ({
   label,
   href,
-  title = '',
+  title,
   columns = 1,
   description,
+  list = [],
   target,
   className,
   children,
   variant = 'outlined',
   ...props
 }) => {
-  const singleCol = columns === 1
+  const withDescription = !!description
+  const withList = list.length > 0
+
+  const display =
+    title && !withDescription && !withList
+      ? 'title-only'
+      : title && description && columns === 2
+      ? `full-width`
+      : title && description && list.length > 0
+      ? 'list'
+      : 'simple'
 
   return (
     <div
       className={clsx(
         className,
         'mdx-cta-section',
-        `mdx-cta-section--cols-${columns}`,
-        description && 'mdx-cta-section--with-description',
+        `mdx-cta-section--${display}`,
       )}
       {...(props as any)}
     >
-      <Typography
-        variant={singleCol && !description ? 'h1' : 'h6'}
-        component="h2"
-        className="mdx-cta-section__title"
-      >
-        {title}
-      </Typography>
-      {description && (
-        <Typography
-          variant={singleCol ? 'h2' : 'h4'}
-          component="h3"
-          className="mdx-cta-section__description"
-        >
+      <div className="mdx-cta-section__container">
+        <Typography component="h2" className="mdx-cta-section__title">
+          {title}
+        </Typography>
+        <Typography component="h3" className="mdx-cta-section__description">
           {description}
         </Typography>
-      )}
-      {href && (
-        <CallToActionButton
-          target={target}
-          href={href}
-          className="mdx-cta-section__link"
-          variant={variant}
-        >
-          {label}
-        </CallToActionButton>
+        {href && (
+          <CallToActionButton
+            target={target}
+            href={href}
+            className="mdx-cta-section__link"
+            variant={variant}
+          >
+            {label}
+          </CallToActionButton>
+        )}
+      </div>
+      {list.length > 0 && (
+        <div className="mdx-cta-section__list">
+          {list.map((option, index) => (
+            <div key={index}>
+              <Typography variant="subtitle1" component="div">
+                {option.title}
+              </Typography>
+              <Typography variant="h3" component="p">
+                {option.description}
+              </Typography>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   )
