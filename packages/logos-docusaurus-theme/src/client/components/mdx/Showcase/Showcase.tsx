398f02557533280da77ccc5f1af0f0e98ea5f5ea
Hossein Mehrabi <ohyeaross@gmail.com>
2023-10-10 16:44:55 +0330

    refactor: use the new Grid in the Showcase component; refs #116

--- packages/logos-docusaurus-theme/src/client/components/mdx/Showcase/Showcase.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/Showcase/Showcase.tsx
@@ -1,71 +1,31 @@
-import { Typography } from '@acid-info/lsd-react'
-import ThemedImage from '@theme/ThemedImage'
 import clsx from 'clsx'
 import React from 'react'
-import { makeStyle } from '../../../lib/makeStyle'
+import { Grid, GridProps, ShowcaseCard, ShowcaseCardProps } from '..'
 import './Showcase.scss'
 
-export type ShowcaseItem = {
-  name: React.ReactNode
-  logo?: string
-  logoDark?: string
-  description: React.ReactNode
-}
-
-export type ShowcaseProps = Omit<
-  React.HTMLAttributes<HTMLDivElement>,
-  'title'
-> & {
-  items: ShowcaseItem[]
-  columns?: number
+export type ShowcaseProps = GridProps & {
+  items: ShowcaseCardProps[]
 }
 
 export const Showcase: React.FC<ShowcaseProps> = ({
   items = [],
-  columns = 4,
-  style = {},
   className,
-  children,
   ...props
 }) => {
   return (
-    <div
+    <Grid
       className={clsx(className, 'mdx-showcase')}
-      style={makeStyle({ ...style }, { columns })}
+      xs={{ cols: 2, wrap: true, gap: '1.5rem 1em' }}
+      lg={{ cols: 3, gap: '1rem' }}
       {...props}
     >
       {items.map((item, index) => {
         return (
-          <div key={index} className="mdx-showcase__item">
-            <div key={index} className="mdx-showcase__item-inner">
-              {item?.logo && item?.logoDark && (
-                <ThemedImage
-                  sources={{
-                    dark: item.logoDark,
-                    light: item.logo,
-                  }}
-                  alt={typeof item.name === 'string' ? item.name : 'image'}
-                  className="mdx-showcase__item-logo"
-                />
-              )}
-              <Typography
-                variant="h3"
-                component="h2"
-                className="mdx-showcase__item-name"
-              >
-                {item.name}
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                component="p"
-                className="mdx-showcase__item-description"
-              >
-                {item.description}
-              </Typography>
-            </div>
-          </div>
+          <Grid.Item key={index} className="mdx-showcase__item" xs={1}>
+            <ShowcaseCard {...item} />
+          </Grid.Item>
         )
       })}
-    </div>
+    </Grid>
   )
 }
