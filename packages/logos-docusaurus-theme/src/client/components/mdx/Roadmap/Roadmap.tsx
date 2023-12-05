4d02df2d938ba865bd1280fa0b7578499b8a8615
jinhojang6 <jinho5051@gmail.com>
2023-12-06 01:39:51 +0900

    feat: update components related to roadmap

--- packages/logos-docusaurus-theme/src/client/components/mdx/Roadmap/Roadmap.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/Roadmap/Roadmap.tsx
@@ -24,6 +24,48 @@ export const Roadmap: React.FC<RoadmapProps> = ({
   children,
   ...props
 }) => {
+  const currentYear = new Date().getFullYear()
+
+  const handleBorderStyle = (year: number, isLast: boolean) => {
+    if (year === currentYear) {
+      return 'solid'
+    } else if (isLast) {
+      return 'none'
+    } else return 'dashed'
+  }
+
+  const getCurrentQuarter = () => {
+    const month = new Date().getMonth()
+    return 'Q' + Math.ceil((month + 1) / 3)
+  }
+
+  const handlePeriodStyle = (period: TimelineItemProps['period']) => {
+    const currentYear = new Date().getFullYear()
+    const currentQuarter = getCurrentQuarter()
+
+    let year, additional
+
+    if (Array.isArray(period)) {
+      ;[year, additional] = period
+    } else {
+      year = period
+    }
+
+    if (year < currentYear) {
+      return 'filled'
+    } else if (year > currentYear) {
+      return 'transparent'
+    } else {
+      if (!additional || additional === '+') {
+        return 'filled'
+      }
+      if (additional <= currentQuarter) {
+        return 'filled'
+      }
+      return 'transparent'
+    }
+  }
+
   return (
     <div
       className={clsx(
@@ -47,17 +89,24 @@ export const Roadmap: React.FC<RoadmapProps> = ({
       {timeline.length > 0 && (
         <Grid
           className="mdx-roadmap__timeline"
-          xs={{ cols: 6, wrap: false, gap: '0 1rem' }}
+          xs={{ cols: 6, wrap: false, gap: '0 1rem', scrollButtons: true }}
+          spacingButtons
+          leftLabel="Past"
+          rightLabel="Future"
         >
           {timeline.map((item, index) => (
             <Grid.Item key={index} xs={1}>
               <TimelineItem
                 {...item}
                 index={item.index ?? index + 1}
-                period={item.period}
+                period={item.period ?? currentYear}
                 description={item.description}
                 alignment={alignment}
-                borderStyle={item.borderStyle}
+                borderStyle={handleBorderStyle(
+                  item?.period?.[0] ?? currentYear,
+                  index === timeline.length - 1,
+                )}
+                periodStyle={handlePeriodStyle(item?.period ?? currentYear)}
                 className={clsx('mdx-roadmap__timeline-item', item.className)}
               />
             </Grid.Item>
