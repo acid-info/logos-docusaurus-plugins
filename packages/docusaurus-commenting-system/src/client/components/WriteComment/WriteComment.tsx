59bcf54034c67d555ef0b7d17ca205271ef95d3e
JulesFILIOT <dev@julesfiliot.com>
2023-12-24 02:47:19 +0100

    feat(commenting system): add input handling logic to WriteComment component

--- packages/docusaurus-commenting-system/src/client/components/WriteComment/WriteComment.tsx
+++ packages/docusaurus-commenting-system/src/client/components/WriteComment/WriteComment.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CardBody, Button } from '@acid-info/lsd-react'
 import styles from './WriteComment.module.scss'
 
 export type WriteCommentProps = {
-  handleSend: () => void
+  handleSend: (comment: string) => void
   handleSignIn: () => void
   isSignedIn?: boolean
 }
@@ -13,6 +13,16 @@ export const WriteComment: React.FC<WriteCommentProps> = ({
   handleSignIn,
   isSignedIn = false,
 }) => {
+  const [comment, setComment] = useState('')
+
+  const onSendClick = () => {
+    const commentToSend = comment.trim()
+    if (commentToSend) {
+      handleSend(commentToSend)
+      setComment('')
+    }
+  }
+
   return (
     <CardBody>
       <textarea
@@ -20,9 +30,15 @@ export const WriteComment: React.FC<WriteCommentProps> = ({
         placeholder={isSignedIn ? 'Write a comment...' : 'Sign in to comment'}
         disabled={!isSignedIn}
         rows={3}
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
       />
       <div className={styles.actionContainer}>
-        <Button size="small" onClick={isSignedIn ? handleSend : handleSignIn}>
+        <Button
+          size="small"
+          onClick={isSignedIn ? onSendClick : handleSignIn}
+          disabled={isSignedIn && comment.length === 0}
+        >
           {isSignedIn ? 'Send' : 'Sign In'}
         </Button>
       </div>
