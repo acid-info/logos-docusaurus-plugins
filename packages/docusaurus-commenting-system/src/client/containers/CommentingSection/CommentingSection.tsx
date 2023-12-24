351a502bd5378905040a1452513dacd923dbba07
JulesFILIOT <dev@julesfiliot.com>
2023-12-24 04:31:29 +0100

    feat(commenting system): add comments handling logic to CommentingSection container

--- packages/docusaurus-commenting-system/src/client/containers/CommentingSection/CommentingSection.tsx
+++ packages/docusaurus-commenting-system/src/client/containers/CommentingSection/CommentingSection.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Comment } from '../../components/Comment'
 import { WriteComment } from '../../components/WriteComment'
+import { signInWithGitHub, getUserData } from '../../../server/auth'
+import {
+  postOneComment,
+  fetchComments,
+  fetchOneComment,
+  subscribeToCommentsInsert,
+  type CommentWithProfile,
+} from '../../../server/comments'
 import styles from './CommentingSection.module.scss'
 
 type CommentData = {
@@ -12,35 +20,81 @@ type CommentData = {
 }
 
 export const CommentingSection: React.FC = () => {
-  const location = useLocation() // location.pathname
+  const location = useLocation()
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
   const [commentsList, setCommentsList] = useState<CommentData[]>([])
 
+  const checkUserSignedIn = async () => {
+    const data = await getUserData()
+    if (data) {
+      setIsSignedIn(true)
+    } else {
+      setIsSignedIn(false)
+    }
+  }
+
+  const fetchCommentsList = async () => {
+    const data = await fetchComments(location.pathname)
+    setCommentsList(
+      data.map((commentFromDB: CommentWithProfile) => ({
+        id: commentFromDB.id,
+        username: commentFromDB.profiles.user_name,
+        comment: commentFromDB.text,
+        date: commentFromDB.created_at,
+      })),
+    )
+  }
+
+  const subscribeToNewComments = () => {
+    const unsubscibe = subscribeToCommentsInsert(async (payload) => {
+      const newCommentId = payload?.new?.id
+      if (!newCommentId) return
+
+      const newComment = await fetchOneComment(newCommentId)
+      if (!newComment) return
+
+      setCommentsList((prev) => [
+        {
+          id: newComment.id,
+          username: newComment.profiles.user_name,
+          comment: newComment.text,
+          date: newComment.created_at,
+        },
+        ...prev,
+      ])
+    })
+
+    return unsubscibe
+  }
+
+  const handleSignIn = async () => {
+    await signInWithGitHub(window.location.href)
+  }
+
+  const handleSend = async (commentText: string) => {
+    await postOneComment(commentText, location.pathname)
+  }
+
   useEffect(() => {
-    setCommentsList([
-      {
-        id: 1,
-        username: 'John Doe',
-        comment:
-          "This is a comment\nAnother line. Trying to make it longer. I have no idea what to write here. I'm just trying to make it longer.",
-        date: '2021-09-01',
-      },
-      {
-        id: 2,
-        username: 'Jane Doe',
-        comment: 'This is another comment',
-        date: '2021-09-02',
-      },
-    ])
+    // check if user is signed in
+    checkUserSignedIn()
+    // fetch all comments on first load
+    fetchCommentsList()
+    // subscribe to new comments
+    const unsubscibe = subscribeToNewComments()
+    return () => {
+      unsubscibe()
+    }
   }, [])
 
   return (
     <div>
       <WriteComment
-        handleSend={() => console.log('Send')}
-        handleSignIn={() => console.log('Sign In')}
-        isSignedIn={true}
+        handleSend={(text) => handleSend(text)}
+        handleSignIn={handleSignIn}
+        isSignedIn={isSignedIn}
       />
-      {commentsList.length && (
+      {commentsList.length > 0 && (
         <>
           <div className={styles.commentsContainer}>
             <span>{`${commentsList.length} comment${
