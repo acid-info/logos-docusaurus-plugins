246363ca1d547d8d680b5c8f0c5342b1794a4079
jinhojang6 <jinho5051@gmail.com>
2024-01-25 22:50:34 +0900

    feat: events and event template page

---
+++ packages/logos-docusaurus-theme/src/client/components/mdx/EventAbout/EventAbout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './EventAbout.scss'
+import { Typography } from '@acid-info/lsd-react'
+
+export type EventAboutProps = React.HTMLProps<HTMLAnchorElement> & {
+  content: string
+}
+
+export const EventAbout: React.FC<EventAboutProps> = ({ children }) => {
+  return (
+    <div className="mdx-event-about__container">
+      <Typography
+        variant="h2"
+        component="h2"
+        className="mdx-event-about__title"
+      >
+        About
+      </Typography>
+      <Typography variant="body1">{children}</Typography>
+    </div>
+  )
+}
---
+++ packages/logos-docusaurus-theme/src/client/components/mdx/EventCard/EventCard.tsx
@@ -0,0 +1,50 @@
+import { Typography } from '@acid-info/lsd-react'
+import ThemedImage from '@theme/ThemedImage'
+import clsx from 'clsx'
+import React from 'react'
+import './EventCard.scss'
+
+export type EventCardProps = React.HTMLProps<HTMLAnchorElement> & {
+  thumbnail: string
+  title: string
+  date: React.ReactNode
+  location: string
+}
+
+export const EventCard: React.FC<EventCardProps> = ({
+  thumbnail,
+  title,
+  date,
+  location,
+  ...props
+}) => {
+  return (
+    <a
+      target="_self"
+      {...props}
+      className={clsx(props.className, 'mdx-event-card')}
+    >
+      <ThemedImage
+        sources={{
+          dark: thumbnail,
+          light: thumbnail,
+        }}
+        alt={title ?? 'event thumbnail'}
+        className="mdx-event-card__thumbnail"
+      />
+      <Typography component="h5" variant="h5" className="mdx-event-card__title">
+        {title}
+      </Typography>
+      <div className="mdx-event-card__row">
+        <div className="mdx-event-card__info">
+          <Typography variant="body2">Date</Typography>
+          <Typography variant="body2">{date}</Typography>
+        </div>
+        <div className="mdx-event-card__info">
+          <Typography variant="body2">Location</Typography>
+          <Typography variant="body2">{location}</Typography>
+        </div>
+      </div>
+    </a>
+  )
+}
