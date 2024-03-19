643796849e6753e33fca197df5c7b3a2d2c0d0a0
jinhojang6 <jinho5051@gmail.com>
2024-03-20 01:34:16 +0900

    feat: implement HeroImage

---
+++ packages/logos-docusaurus-theme/src/client/components/mdx/HeroImage/HeroImage.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+import React, { PropsWithChildren } from 'react'
+import './HeroImage.scss'
+
+export type HeroImageProps = PropsWithChildren<
+  React.HTMLAttributes<HTMLDivElement>
+> & {
+  src: string
+  alt: string
+}
+
+export const HeroImage: React.FC<HeroImageProps> = ({
+  src,
+  alt,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <>
+      <div className={clsx(className, 'mdx-hero-image')} {...(props as any)}>
+        <img src={src} alt={alt} className="mdx-hero-image__img" />
+        {children}
+      </div>
+    </>
+  )
+}
