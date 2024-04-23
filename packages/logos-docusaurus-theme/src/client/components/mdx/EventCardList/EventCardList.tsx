11d8059821a0715ddb1c53f0bc32f05065b21749
jinhojang6 <jinho5051@gmail.com>
2024-04-24 01:30:20 +0900

    feat: support current/past status for events

--- packages/logos-docusaurus-theme/src/client/components/mdx/EventCardList/EventCardList.tsx
+++ packages/logos-docusaurus-theme/src/client/components/mdx/EventCardList/EventCardList.tsx
@@ -2,17 +2,39 @@ import React, { useState } from 'react'
 import './EventCardList.scss'
 import { Button } from '@acid-info/lsd-react'
 import { Box, EventCard, EventCardProps, Grid } from '..'
+import { TabItem, Tabs } from '@acid-info/lsd-react'
 
 export type EventCardListProps = {
-  data: EventCardProps[]
+  current: EventCardProps[]
+  past: EventCardProps[]
 }
 
-export const EventCardList: React.FC<EventCardListProps> = ({ data }) => {
-  const [showMore, setShowMore] = useState<boolean>(data?.length > 6)
-  const [count, setCount] = useState<number>(6)
+export enum EventStatus {
+  CURRENT = 'Current',
+  PAST = 'Past',
+}
+
+const LIMIT = 8
+
+export const EventCardList: React.FC<EventCardListProps> = ({
+  current,
+  past,
+  ...props
+}) => {
+  const [activeTab, setActiveTab] = useState<EventStatus>(EventStatus.CURRENT)
+  const data = activeTab === EventStatus.CURRENT ? current : past
+
+  const [showMore, setShowMore] = useState<boolean>(data?.length > LIMIT)
+  const [count, setCount] = useState<number>(LIMIT)
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab as EventStatus)
+    setCount(LIMIT)
+    setShowMore(data?.length > LIMIT)
+  }
 
   const handleShowMore = () => {
-    setCount((prev) => prev + 6)
+    setCount((prev) => prev + LIMIT)
 
     if (count + 6 >= data?.length) {
       setShowMore(false)
@@ -20,25 +42,39 @@ export const EventCardList: React.FC<EventCardListProps> = ({ data }) => {
   }
 
   return (
-    <div className="mdx-event-card-list__container">
-      <Box top={56} bottom={80}>
-        <Grid xs={{ cols: 1, gap: '80px 16px' }} md={{ cols: 2 }}>
-          {data.slice(0, count).map((event, index) => (
-            <Grid.Item xs={1} key={index}>
-              <EventCard {...event} />
-            </Grid.Item>
-          ))}
-        </Grid>
-      </Box>
-      {showMore && (
-        <Button
-          className="mdx-event-card-list__button"
-          onClick={handleShowMore}
-          size="large"
-        >
-          See more
-        </Button>
-      )}
+    <div className="mdx-event-card-list__container" {...props}>
+      <Tabs
+        activeTab={activeTab}
+        onChange={handleTabChange}
+        className="mdx-event-card-list__tabs"
+      >
+        <TabItem key={'current'} name={`Current`}>
+          {`Current`}
+        </TabItem>
+        <TabItem key={'past'} name={`Past`}>
+          {`Past`}
+        </TabItem>
+      </Tabs>
+      <div>
+        <Box top={{ xs: 80, sm: 96 }} bottom={80}>
+          <Grid xs={{ cols: 1, gap: '80px 16px' }} md={{ cols: 4 }}>
+            {data.slice(0, count).map((event, index) => (
+              <Grid.Item xs={1} key={index}>
+                <EventCard {...event} />
+              </Grid.Item>
+            ))}
+          </Grid>
+        </Box>
+        {showMore && (
+          <Button
+            className="mdx-event-card-list__button"
+            onClick={handleShowMore}
+            size="large"
+          >
+            See more
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
