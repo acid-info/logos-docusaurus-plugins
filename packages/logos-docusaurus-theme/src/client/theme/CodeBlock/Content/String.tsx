bc825c6d8a3768f6fd0371ee0df734b367578e7b
jinhojang6 <jinho5051@gmail.com>
2025-07-16 21:43:32 +0900

    feat: upgrade docusaurus version to 3.8.1

--- packages/logos-docusaurus-theme/src/client/theme/CodeBlock/Content/String.tsx
+++ packages/logos-docusaurus-theme/src/client/theme/CodeBlock/Content/String.tsx
@@ -1,106 +1,35 @@
-import React, { ReactElement } from 'react'
-import clsx from 'clsx'
-import { useThemeConfig, usePrismTheme } from '@docusaurus/theme-common'
+import React, { type ReactNode } from 'react'
+import { useThemeConfig } from '@docusaurus/theme-common'
 import {
-  parseCodeBlockTitle,
-  parseLanguage,
-  parseLines,
-  containsLineNumbers,
+  CodeBlockContextProvider,
+  type CodeBlockMetadata,
+  createCodeBlockMetadata,
   useCodeWordWrap,
 } from '@docusaurus/theme-common/internal'
-import { Highlight } from 'prism-react-renderer'
-import Line from '@theme/CodeBlock/Line'
-import CopyButton from '@theme/CodeBlock/CopyButton'
-import WordWrapButton from '@theme/CodeBlock/WordWrapButton'
-import Container from '../Container'
 import type { Props } from '@theme/CodeBlock/Content/String'
+import CodeBlockLayout from '@theme/CodeBlock/Layout'
 
-import styles from './styles.module.css'
-import { Typography } from '@acid-info/lsd-react'
-
-export default function CodeBlockString({
-  children,
-  className: blockClassName = '',
-  metastring,
-  title: titleProp,
-  showLineNumbers: showLineNumbersProp,
-  language: languageProp,
-}: Props): ReactElement {
-  const {
-    prism: { defaultLanguage, magicComments },
-  } = useThemeConfig()
-  const language =
-    languageProp ?? parseLanguage(blockClassName) ?? defaultLanguage
-  const prismTheme = usePrismTheme()
-  const wordWrap = useCodeWordWrap()
-
-  // We still parse the metastring in case we want to support more syntax in the
-  // future. Note that MDX doesn't strip quotes when parsing metastring:
-  // "title=\"xyz\"" => title: "\"xyz\""
-  const title = parseCodeBlockTitle(metastring) || titleProp
-
-  const { lineClassNames, code } = parseLines(children, {
-    metastring,
-    language,
-    magicComments,
+function useCodeBlockMetadata(props: Props): CodeBlockMetadata {
+  const { prism } = useThemeConfig()
+  return createCodeBlockMetadata({
+    code: props.children,
+    className: props.className,
+    metastring: props.metastring,
+    magicComments: prism.magicComments,
+    defaultLanguage: prism.defaultLanguage,
+    language: props.language,
+    title: props.title,
+    showLineNumbers: props.showLineNumbers,
   })
-  const showLineNumbers = showLineNumbersProp ?? containsLineNumbers(metastring)
+}
 
+// TODO Docusaurus v4: move this component at the root?
+export default function CodeBlockString(props: Props): ReactNode {
+  const metadata = useCodeBlockMetadata(props)
+  const wordWrap = useCodeWordWrap()
   return (
-    <Container
-      as="div"
-      className={clsx(
-        blockClassName,
-        language &&
-          !blockClassName.includes(`language-${language}`) &&
-          `language-${language}`,
-      )}
-    >
-      {title && (
-        <div className={styles.codeBlockTitle}>
-          <Typography variant="subtitle1">{title}</Typography>
-        </div>
-      )}
-      <div className={styles.codeBlockContent}>
-        <Highlight theme={prismTheme} code={code} language={language ?? 'text'}>
-          {({ className, tokens, getLineProps, getTokenProps }) => (
-            <pre
-              /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
-              tabIndex={0}
-              ref={wordWrap.codeBlockRef}
-              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}
-            >
-              <code
-                className={clsx(
-                  styles.codeBlockLines,
-                  showLineNumbers && styles.codeBlockLinesWithNumbering,
-                )}
-              >
-                {tokens.map((line, i) => (
-                  <Line
-                    key={i}
-                    line={line}
-                    getLineProps={getLineProps}
-                    getTokenProps={getTokenProps}
-                    classNames={lineClassNames[i]}
-                    showLineNumbers={showLineNumbers}
-                  />
-                ))}
-              </code>
-            </pre>
-          )}
-        </Highlight>
-        <div className={styles.buttonGroup}>
-          {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
-            <WordWrapButton
-              className={styles.codeButton}
-              onClick={() => wordWrap.toggle()}
-              isEnabled={wordWrap.isEnabled}
-            />
-          )}
-          <CopyButton className={styles.codeButton} code={code} />
-        </div>
-      </div>
-    </Container>
+    <CodeBlockContextProvider metadata={metadata} wordWrap={wordWrap}>
+      <CodeBlockLayout />
+    </CodeBlockContextProvider>
   )
 }
